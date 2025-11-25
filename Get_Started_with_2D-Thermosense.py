3417214ce4407f3486daee89e472ca4688d01f84
Upendra Arun <upendra.arun@tum.de>
2025-11-25 19:07:13 +0100

    updated thermal cam stream

--- Get_Started_with_2D-Thermosense.py
+++ Get_Started_with_2D-Thermosense.py
@@ -1,4 +1,5 @@
 # Sample code for using the 2D-Thermosense
+import sys
 
 # Importing various libraries/packages
 import senxor.proc
@@ -39,33 +40,40 @@ except Exception as e:
 
 dev.start_stream() # Start the data stream from the device
 
-header, frame = dev.read() # Read a single frame from the stream
 
-# Apply offset correction, if set
-if frame is not None:
-    if offset_value != 0:
-        frame = frame + offset_value
+while (1):
+    header, frame = dev.read() # Read a single frame from the stream
 
-    print(f"Frame min: {frame.min()} {temp_unit}, max: {frame.max()} {temp_unit}, mean: {frame.mean():.1f} {temp_unit}") # Print min, max and mean temperature values from the frame
-    #np.set_printoptions(threshold=np.inf)  # When deleting the hashtag, all values are displayed in the console
-    np.set_printoptions(precision=1, floatmode='fixed') # Set number format for printing
-    print(frame)  # Output raw data from the thermal image
-    np.savetxt("thermal_frame.csv", frame.astype(np.float32), delimiter=",", fmt="%.2f")  # Save the raw data as CSV, number format with 2 decimal places
+    # Apply offset correction, if set
+    if frame is not None:
+        if offset_value != 0:
+            frame = frame + offset_value
+        
+        #print(frame[0][0].dtype)
+        #print(f"Frame min: {frame.min()} {temp_unit}, max: {frame.max()} {temp_unit}, mean: {frame.mean():.1f} {temp_unit}") # Print min, max and mean temperature values from the frame
+        #np.set_printoptions(threshold=np.inf)  # When deleting the hashtag, all values are displayed in the console
+        np.set_printoptions(precision=1, floatmode='fixed') # Set number format for printing
+        #print(frame)  # Output raw data from the thermal image
+        np.savetxt("thermal_frame.csv", frame.astype(np.float32), delimiter=",", fmt="%.2f")  # Save the raw data as CSV, number format with 2 decimal places
 
-    uint8_image = senxor.proc.normalize(frame, dtype=np.uint8) # Normalize the frame to uint8 for visualization
-    float32_image = senxor.proc.normalize(frame, dtype=np.float32) # Normalize the frame to float32 for further processing
-    enlarged_image = senxor.proc.enlarge(uint8_image, scale=5)  # Enlarge image by a factor of 5
-    cmap_cv_inferno = senxor.proc.get_colormaps("inferno", "cv") # Get the inferno colormap in OpenCV format
-    colored_image = senxor.proc.apply_colormap(enlarged_image, lut=cmap_cv_inferno) # Coloring the image
+        uint8_image = senxor.proc.normalize(frame, dtype=np.uint8) # Normalize the frame to uint8 for visualization
+        float32_image = senxor.proc.normalize(frame, dtype=np.float32) # Normalize the frame to float32 for further processing
+        enlarged_image = senxor.proc.enlarge(uint8_image, scale=5)  # Enlarge image by a factor of 5
+        cmap_cv_inferno = senxor.proc.get_colormaps("inferno", "cv") # Get the inferno colormap in OpenCV format
+        #colored_image = senxor.proc.apply_colormap(float32_image, lut=cmap_cv_inferno) # Coloring the image
+        colored_image = senxor.proc.apply_colormap(enlarged_image, lut=cmap_cv_inferno)
 
-    # OpenCV displays images in BGR format
-    bgr_image = cv2.cvtColor(colored_image, cv2.COLOR_RGB2BGR)
+        # OpenCV displays images in BGR format
+        bgr_image = cv2.cvtColor(colored_image, cv2.COLOR_RGB2BGR)
 
-    bgr_image = cv2.flip(bgr_image, 1) # Flip image
-    cv2.imshow("senxor", bgr_image) # Display the image using OpenCV, if the line is commented the image window will not open
-    cv2.waitKey(0) # Wait for a key press to close the image window
-    cv2.destroyAllWindows() # Close all OpenCV windows
-else:
-    print("No frame received!")
+        bgr_image = cv2.flip(bgr_image, 1) # Flip image
+        #print(bgr_image.shape)
+        cv2.imshow("senxor", bgr_image) # Display the image using OpenCV, if the line is commented the image window will not open
+        cv2.waitKey(1) # Wait for a key press to close the image window
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        #cv.destroyAllWindows() # Close all OpenCV windows
+    else:
+        print("No frame received!")
 
 dev.close() # Stop the stream and close the connection
